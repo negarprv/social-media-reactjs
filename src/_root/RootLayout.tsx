22910bf49072c2e65c53e289fc4bffd5effbe20f
Negar Parvaresh <negar.prv@gmail.com>
2023-12-19 01:48:46 +0330

    topbar and leftside completed

--- src/_root/RootLayout.tsx
+++ src/_root/RootLayout.tsx
@@ -1,7 +1,21 @@
-import React from "react";
+import BottomBar from "@/components/shared/BottomBar";
+import LeftSidebar from "@/components/shared/LeftSidebar";
+import Topbar from "@/components/shared/Topbar";
+import { Outlet } from "react-router-dom";
 
 function RootLayout() {
-  return <div>rootlayout</div>;
+  return (
+    <div className=" w-full md:flex">
+      <Topbar />
+      <LeftSidebar />
+
+      <section className=" flex flex-1 h-full">
+        <Outlet />
+      </section>
+
+      <BottomBar />
+    </div>
+  );
 }
 
 export default RootLayout;

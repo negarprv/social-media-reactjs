45b4abdebc78dbb910366f26b9ed8c4c3aef443c
Negar Parvaresh <negar.prv@gmail.com>
2024-01-03 15:59:59 +0330

    allUsers created

--- src/_root/pages/AllUsers.tsx
+++ src/_root/pages/AllUsers.tsx
@@ -1,7 +1,30 @@
+import UserCard from "@/components/shared/UserCard";
+import { useGetUsers } from "@/lib/react-query/queriesAndMutations";
+import { Models } from "appwrite";
+import { Loader } from "lucide-react";
 import React from "react";
 
 const AllUsers = () => {
-  return <div>AllUsers</div>;
+  const {
+    data: users,
+    isPending: isGetUserLoading,
+    isError: isGetUserError,
+  } = useGetUsers(10);
+  return (
+    <div className=" allUsers">
+      <h3 className=" h3-bold">All Users</h3>
+
+      {isGetUserLoading && !users ? (
+        <Loader />
+      ) : (
+        <ul className="grid grid-cols-5  items-center gap-5 ">
+          {users?.documents.map((user: Models.Document) => (
+            <UserCard key={user.$id} user={user} />
+          ))}
+        </ul>
+      )}
+    </div>
+  );
 };
 
 export default AllUsers;
